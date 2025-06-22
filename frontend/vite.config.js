64abd32ead51787ce36d60d6f98271b69ea6fbac
misterxcrypt <misterxcrypt@gmail.com>
2025-06-23 01:32:07 +0530

    start

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/linux-rice-catalogue/', // <-- your repo name with trailing slash
+  base: './', // <-- your repo name with trailing slash
 })
 

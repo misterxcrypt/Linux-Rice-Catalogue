19b3c5d8bb4356e3be7b914994afb13373fe21ba
misterxcrypt <misterxcrypt@gmail.com>
2025-06-23 12:11:38 +0530

    start

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- your repo name with trailing slash
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // important for correct routing
 })
-
